fbe40672cffb49cc6cf129fb3792a401cf029eb1
naveenoo9 <50537003+naveenoo9@users.noreply.github.com>
2025-02-04 20:52:58 +0530

    Updated configs

---
+++ Read.md
@@ -0,0 +1,5 @@
+## Important URL's
+
+#### Swagger UI
+
+- http://localhost:9092/swagger-ui/index.html
--- src/main/java/com/personal/cards/CardsApiApplication.java
+++ src/main/java/com/personal/cards/CardsApiApplication.java
@@ -2,7 +2,7 @@ package com.personal.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,5 +12,4 @@ public class CardsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CardsApiApplication.class, args);
 	}
-
 }
